d90d1662213dd40d7dcf01363a7fe8ce5842682c
mathias87sf <mathias87sf@gmail.com>
2021-10-14 16:36:52 -0300

    Refatoração
    Refatorado para que a composição seja feita de modo a utilizar apenas um ArrayList de objetos ElementoDeComposicao, ao invés de um ArrayList para cada tipo.

---
+++ ElementoDeComposicao.java
@@ -0,0 +1,8 @@
+public abstract class ElementoDeComposicao {
+
+    public abstract Composicao getComposicao();
+    public abstract void setComposicao(Composicao composicao);
+
+    public abstract int getIdentificador();
+    public abstract double getCapacidade();
+}
